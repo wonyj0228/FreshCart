e036858bece3ccb4096a5aa6ef4b4ff7bd3686bb
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-12-03 23:41:47 +0900

    scss, reset css 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Components/Header";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./Routes/Home";
-import Price from "./Routes/Price";
-import History from "./Routes/History";
-import Login from "./Routes/Login";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './assets/css/styles.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './routes/Home';
+import Price from './routes/Price';
+import History from './routes/History';
+import Login from './routes/Login';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/price", element: <Price /> },
-      { path: "/history", element: <History /> },
-      { path: "/login", element: <Login /> },
+      { path: '/', element: <Home /> },
+      { path: '/price', element: <Price /> },
+      { path: '/history', element: <History /> },
+      { path: '/login', element: <Login /> },
     ],
   },
 ]);
