dfdc77069d670a5b713978e9c44b553393c5e689
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-12-07 21:07:32 +0900

    header scroll 이벤트 추가

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,25 +1,39 @@
 import { Link, useLocation } from 'react-router-dom';
 import { FaCircleUser } from 'react-icons/fa6';
 import { useEffect, useRef, useState } from 'react';
+import { useMotionValueEvent, useScroll } from 'framer-motion';
 
 function Header() {
   const { pathname } = useLocation();
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(true);
   const ref = useRef<HTMLElement>(null);
+  const { scrollY } = useScroll();
 
   useEffect(() => {
-    if (pathname === '/' || pathname === '/price') {
-      setTheme(false);
-    } else {
+    if (pathname === '/' || pathname == '/price') {
       setTheme(true);
+    } else {
+      setTheme(false);
     }
   }, [pathname]);
+
+  useMotionValueEvent(scrollY, 'change', () => {
+    if (theme) {
+      if (ref.current) {
+        if (scrollY.get() >= 75) {
+          ref.current.className = 'header__theme-default';
+        } else {
+          ref.current.className = 'header__theme-inherit';
+        }
+      }
+    }
+  });
 
   return (
     <header
       id="header"
       ref={ref}
-      className={`header__theme-${theme ? 'white' : 'default'}`}
+      className={theme ? 'header__theme-inherit' : 'header__theme-default'}
     >
       <div className="header__logo">
         <a href="/">
--- src/Routes/Price.tsx
+++ src/Routes/Price.tsx
@@ -1,5 +1,29 @@
+import { FaSearch, FaFire } from 'react-icons/fa';
+import priceImg from '../assets/images/priceImg.png';
+
 function Price() {
-  return <h1>Price</h1>;
+  return (
+    <div className="price__wrapper">
+      <div className="price__bg">
+        <img src={priceImg} className="price__img" />
+      </div>
+
+      <div className="price__search">
+        <input placeholder="품목을 입력해주세요" />
+        <FaSearch />
+        <div className="price__search-hot">
+          <span>HOT</span>
+          <FaFire />
+          <span>고구마</span>
+          <span>사과</span>
+          <span>토마토</span>
+          <span>쌀</span>
+          <span>고춧가루</span>
+        </div>
+      </div>
+      <div className="price_layout"></div>
+    </div>
+  );
 }
 
 export default Price;
